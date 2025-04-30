7f2f8c4774380c7c7b708e3368e3c30e451f349a
Aditya Ajay <aditya.ajay@tothenew.com>
2025-04-30 12:11:39 +0530

    updated setup config files for distribution

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-from setuptools import setup, find_packages, os
+from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-exec(open(os.path.join(here, 'ckssocli/version.py')).read())
-
-
-setup(
-    name='ck-sso-cli',
-    version=__version__,
-    description='Provides a wrapper for AWS IAM Identity Center authentication to an External AWS Account',
-    packages=find_packages(),
-    license='Apache License 2.0',
-    author='Aditya Ajay',
-    author_email='aditya.ajay@cloudkeeper.com',
-    url='https://www.cloudkeeper.com/',
-    entry_points={
-        'console_scripts': [
-            'ck-sso-cli=ckssocli.ck_sso_cli:main',
-        ],
-    },
-    install_requires=[
-        'boto3'
-        ]
-)
-
-# export PATH="/home/aditya/.local/bin:$PATH"
+setup()
